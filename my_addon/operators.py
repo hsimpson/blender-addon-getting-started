aedde53e7c54eda5ba8d1b915b77707f4eb2263d
Daniel Toplak <homer.simpson123@gmx.de>
2021-08-05 22:44:29 +0200

    feat: implement the mesh creation

--- my_addon/operators.py
+++ my_addon/operators.py
@@ -1,5 +1,6 @@
 """ My Addon operators """
 
+import bpy
 from bpy.types import Operator
 
 
@@ -16,5 +17,21 @@ class MyAddon_OT_my_operator(Operator):
 
         print("mesh type: {}".format(my_addon.mesh_type))
         print("at cursor: {}".format(my_addon.at_3d_cursor))
+
+        loc = (0, 0, 0)
+        if my_addon.at_3d_cursor is True:
+            loc = bpy.context.scene.cursor.location
+
+        if my_addon.mesh_type == '0':
+            # create cube
+            bpy.ops.mesh.primitive_cube_add(align='WORLD', location=loc)
+        elif my_addon.mesh_type == '1':
+            # create sphere
+            bpy.ops.mesh.primitive_uv_sphere_add(align='WORLD', location=loc)
+        elif my_addon.mesh_type == '2':
+            # create Monkey
+            bpy.ops.mesh.primitive_monkey_add(align='WORLD', location=loc)
+        else:
+            print("wrong mesh type!")
 
         return {'FINISHED'}
